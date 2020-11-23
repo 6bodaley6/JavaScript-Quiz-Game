3484f0eea4ea1d35351a68a4ef9dfcca35513449
Bo Daley <70559889+6bodaley6@users.noreply.github.com>
2020-11-22 18:16:40 -0700

    creating skeleton  outline

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,7 +1,60 @@
-var questions = [
-    { q: 'The sky is blue.', a: 't' },
-    { q: 'There are 365 days in a year.', a: 't' },
-    { q: 'There are 42 ounces in a pound.', a: 'f' },
-    { q: 'The Declaration of Independence was created in 1745.', a: 'f' },
-    { q: 'Bananas are vegetables.', a: 'f' }
-  ];
+//!! in questions.js make questions multiple choice and keep track of right answer var questions =
+// let questionsList = [
+//   { q: "Which of the following uses true or false logic.", a: "booleans" },
+//   { q: "The Condition if/else is enclosed within.", a: "curly brackets" },
+//   { q: "Arrays in JavaScript can be used to store", a: "All of the above" },
+//   { q: "String values are supposed to be stored" , a: "parethensis" },
+//   { q: "A very useful tool used during devolopment is used for printing content on to the  debugger is", a: "console.log" },
+// ];
+//!! Iterate over the questions array and display each question in a confirmation box
+//* for (var index = 0; i < questions.length; index++) {
+
+// I to have some logic added that will have event listeners that , And in that event listener that will happen when the user clicks answer that is where 1., 2., 3.,
+// we have to build a loop different from thursday because we do not have the window loop which makes break points
+// we have to use intervals
+// we have to execute different things at different steps
+//
+//!! Get the user response from the question
+//!! Check the user's answer against the correct answer
+//!! Alert the user if they are correct or wrong. Increment the score accordingly
+// //!!add time if answer is correct
+
+//??|question "Commonly used data types DO not include"
+//  |answers"1.Strings 2.booleans 3.alerts 4.numbers"
+//* |correctAnswer = "2.booleans"
+
+//??|question "The Condition if/else is enclosed within"
+//  |answers "1.quotes 2.curly brackets 3.parethensis 4.square brackets"
+//* |correctAnswer = "2.curly brackets"
+
+//??|question "Arrays in JavaScript can be used to store"
+//  |answers "1.numbers and strings 2.other arrays 3.booleans 4.All of the above"
+//* |correctAnswer = ".4 All of the above"
+
+//??|question "String values are supposed to be stored"
+//  |answers 1.commas 2.curly brackets 3.quotes 4.parethensis
+//* |correctAnswer = "4.parethensis"
+
+//??|question "A very useful tool used during devolopment is used for printing content on to the  debugger is"
+//  |answers "1.JavaScript 2.terminal/bash 3.for loops 4.console.log"
+//* |correctAnswer = "console.log"
+
+//
+//!!
+
+//??
+// var askQuestionEl = document.createElement("li");
+// askQuestionEl.textContent = "";
+// askQuestionEl.appendChild(asking-questionsEl);
+// var askQuestionOneEl = document.createElement("li");
+// askQuestionEl.textContent = "";
+// 1
+// var askQuestionTwoEl = document.createElement("li");
+// askQuestionEl.textContent = "";
+
+// var askQuestionThreeEl = document.createElement("li");
+// askQuestionEl.textContent = "";
+
+// var askQuestionEl = document.createElement("li");
+// taskItemEl.textContent = "";
+//!!
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,64 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
+//!!game state
 var quizScore = 0;
 var currentQuestion = 0;
+//
+//!!queryselectors
+var startButton = document.querySelector("#start-button");
+var questionEl = document.querySelector("#question");
+var startEl = document.querySelector("#start");
+var questionsDiv = document.querySelector("#questions");
+//
+//!! function for start button
+function startQuiz() {
+  console.log("CONSOLELOGA1");
+  startEl.setAttribute("class", "hide");
+  questionsDiv.removeAttribute("class");
+  nextQuestion();
+}
+//
+startButton.addEventListener("click", startQuiz);
+
+//!!function that is going to take care of handeling the next question
+function nextQuestion() {
+  console.log("CONSOLELOGA2");
+  var question = questions[currentQuestion].q;
+  questionEl.textContent = question;
+}
+//!!function for time left on quiz
+clockTimeLeftEl.innerText = `time: ${counter}`
+function timeLeftCountdown() {
+    var counter = 90
+    var clockTimeLeftEl = document.createElement.("div")
+    clockTimeLeftEl.innerText = `time: ${counter}`
+    var LeftCountdown  = setInterval(function(){
+            counter--;
+            if(counter === 0) {
+            clearInterval()
+            }
+        }, 1000);   
+    
+}
+//!!function for to take away time if question is answered wrong
+// REFERENCEvar askQuestionEl = document.createElement("li");
+
+
+//TODO inside of next quesion we want to give each answer a button and put it on the screen.
+
+//TODO each button should have  click listener that handles answering a question
+
+//TODO the click listener should handles scores and penalties then handle moving on to the next question
+
+//TODO if there are nomore questions the listener should call a function that handles ending the game
+
+//TODO random tasks timers, scores, stuff like that
