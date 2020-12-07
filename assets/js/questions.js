c87cfbfd0fae21185efbf6bbec1f25767ecd1e7c
Bo Daley <70559889+6bodaley6@users.noreply.github.com>
2020-12-06 17:52:22 -0700

    added functions loadQuestion, restartButton, navigateQuiz, endQuiz, resultEl got my buttons to work and restart quiz if the user selects to restart now I need to interpret if they got the correct answer or not and either add to their score or remove time and allow the user to save score and have it be on local storage

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,25 +1,36 @@
 //!! in questions.js make questions multiple choice and keep track of right answer var questions =
-let questionsList = [
-  { q: "Which of the following uses true or false logic.", a: "booleans" },
-  { q: "The Condition if/else is enclosed within.", a: "curly brackets" },
-  { q: "Arrays in JavaScript can be used to store", a: "All of the above" },
-  { q: "String values are supposed to be stored", a: "parethensis" },
-  {
-    q:
-      "A very useful tool used during devolopment is used for printing content on to the  debugger is",
-    a: "console.log",
-  },
-];
+// let questionsList = [
+//   { q: "Which of the following uses true or false logic.", a: "booleans" },
+//   { q: "The Condition if/else is enclosed within.", a: "curly brackets" },
+//   { q: "Arrays in JavaScript can be used to store", a: "All of the above" },
+//   { q: "String values are supposed to be stored", a: "parethensis" },
+//   {
+//     q:
+//       "A very useful tool used during devolopment is used for printing content on to the  debugger is",
+//     a: "console.log",
+//   },
 //!! Iterate over the questions array and display each question in a confirmation box
 //Answers Array
-let answerList = [
-  ["Strings", "booleans", "alerts", "numbers"],
-  ["quotes", "curly brackets", "parethensis", "square brackets"],
-  ["numbers and strings", "other arrays", "booleans", "All of the above"],
-  ["commas", "curly brackets", "quotes", "parethensis"],
-  ["JavaScript", "terminal/bash", "for loops", "console.log"],
+//TODO come back and fill out
+// let answerList = [
+//   ["Strings", "booleans", "alerts", "numbers"],
+//   ["quotes", "curly brackets", "parethensis", "square brackets"],
+//   ["numbers and strings", "other arrays", "booleans", "All of the above"],
+//   ["commas", "curly brackets", "quotes", "parethensis"],
+//   ["JavaScript", "terminal/bash", "for loops", "console.log"],
+// ];
+let questions = [
+  {
+    text: "Which of the following uses true or false logic?",
+    correctAnswer: "booleans",
+    options: ["Strings", "booleans", "alerts", "numbers"],
+  },
+  {
+    text: "The Condition if/else is enclosed within.",
+    correctAnswer: "curly brackets",
+    options: ["quotes", "curly brackets", "parethensis", "square brackets"],
+  },
 ];
-
 //* for (var index = 0; index < questions.length; index++) {
 
 // I to have some logic added that will have event listeners that , And in that event listener that will happen when the user clicks answer that is where 1., 2., 3.,
@@ -51,23 +62,3 @@ let answerList = [
 //??|question "A very useful tool used during devolopment is used for printing content on to the  debugger is"
 //  |answers "1.JavaScript 2.terminal/bash 3.for loops 4.console.log"
 //* |correctAnswer = "console.log"
-
-//
-//!!
-
-//??
-// var askQuestionEl = document.createElement("li");
-// askQuestionEl.textContent = "";
-// askQuestionEl.appendChild(asking-questionsEl);
-// var askQuestionOneEl = document.createElement("li");
-// askQuestionEl.textContent = "";
-// 1
-// var askQuestionTwoEl = document.createElement("li");
-// askQuestionEl.textContent = "";
-
-// var askQuestionThreeEl = document.createElement("li");
-// askQuestionEl.textContent = "";
-
-// var askQuestionEl = document.createElement("li");
-// taskItemEl.textContent = "";
-//!!
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,3 @@
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and score
 //!!game state
 var quizScore = 0;
 var currentQuestion = 0;
@@ -18,27 +7,97 @@ var startButton = document.querySelector("#start-button");
 var questionEl = document.querySelector("#question");
 var startEl = document.querySelector("#start");
 var questionsDiv = document.querySelector("#questions");
+var answerButtons;
+var resultEL = document.getElementById("end");
+var restartButton = document.querySelector("#restart-button");
+
 //
-//!! function for start button
+//!! function for start buttone
 function startQuiz() {
   console.log("CONSOLELOGA1");
   startEl.setAttribute("class", "hide");
   questionsDiv.classList.remove("hide");
-  nextQuestion();
-  showOptions();
+
+  loadQuestion();
+
+  // nextQuestion();
+  // showOptions();
+  timeLeftCountdown();
 }
-//
+function endQuiz() {
+  questionsDiv.setAttribute("class", "hide");
+  resultEL.classList.remove("hide");
+  startButton.classList.remove("hide");
+  currentQuestion = 0;
+}
+//help me navigate quiz
+function navigateQuiz() {
+  if (currentQuestion < questions.length) {
+    loadQuestion();
+  } else {
+    endQuiz();
+  }
+}
+
 startButton.addEventListener("click", startQuiz);
+restartButton.addEventListener("click", restartQuiz);
+function restartQuiz() {
+  currentQuestion = 0;
+
+  startEl.setAttribute("class", "hide");
+  questionsDiv.classList.remove("hide");
+  resultEL.setAttribute("class", "hide");
+
+  loadQuestion();
+
+  // nextQuestion();
+  // showOptions();
+  timeLeftCountdown();
+}
+
 var counter = 90;
-//TODO (MAKE QUERY SELECTOR) clockTimeLeftEl.innerText = `time: ${counter}`;
+// // //TODO (MAKE QUERY SELECTOR) clockTimeLeftEl.innerText = `time: ${counter}`;
 
 //!!function that is going to take care of handeling the next question
-function nextQuestion() {
+function loadQuestion() {
   console.log("CONSOLELOGA2");
-  var question = questionsList[currentQuestion].q;
-  questionEl.textContent = question;
+  //debugger
+  var question = questions[currentQuestion];
+  questionEl.textContent = question.text;
+  console.log(question.options);
+  var answerButtons = document.getElementById("answerButtons");
+  answerButtons.innerHTML = "";
+  question.options.forEach((answer) => {
+    answerButtons.innerHTML += `<button onclick="javascript:checkQuestion('${answer}')" class="buttonClass" id="${answer}">${answer}</button>`;
+  });
+
+  //  var jQueoryTest = $("test");
+  // var ES6String = `${}
+  //?? var answers = answerList[currentQuestion];
+  //?? console.log(answers);
+  //?? for (var i = 0; i < questionsList.length; i++) {
+  //??   answerButtons = document.createElement("button");
+  //??   console.log(answerButtons);
+  //??   answerButtons.textContent = answers[i];
+  //??   answerButtons.onclick = document //function to be made later it is going to check if somthing is correct if it is () => checkQuestion(answers[i])
+  //??     .querySelector("#answerButtons")
+  //??     .appendChild(answerButtons)
+  //??     .classList.add("buttonClass");
+  //??   console.log("CONSOLELOGA4");
 }
-//!!function for time left on quiz
+function checkQuestion(choice) {
+  console.log("user selected choice: " + choice);
+  let question = questions[currentQuestion];
+  if (question.correctAnswer === choice) {
+    currentQuestion++;
+    navigateQuiz();
+  } else {
+    //?? console.log("incorrect answer" + choice ) //old ass shit
+    console.log(`incorrect answer ${choice}`); //ES6
+    //if they are right what
+  }
+}
+//!!function for timer which shows how much time left on quiz
 function timeLeftCountdown() {
   document.createElement("div").innerText = `time: ${counter}`;
   //// clockTimeLeftEl.innerText = `time: ${counter}`
@@ -49,33 +108,45 @@ function timeLeftCountdown() {
     }
   }, 1000);
 }
-
 //!!function for to take away time if question is answered wrong
-
+//!! function to display questions from array in questions.js
 function showOptions() {
   console.log("CONSOLELOGA3");
   var answers = answerList[currentQuestion];
   console.log(answers);
   for (var i = 0; i < questionsList.length; i++) {
     // document.querySelector(`#answer${i}`).textContent = answers[i];
-    var answer_buttons = document.createElement("button");
-    console.log(answer_buttons);
-    answer_buttons.textContent = answers[i];
-    answer_buttons.onclick = document //function to be made later it is going to check if somthing is correct if it is () => checkQuestion(answers[i])
-      .querySelector("#answer_buttons")
-      .appendChild(answer_buttons);
+    answerButtons = document.createElement("button");
+    console.log(answerButtons);
+    answerButtons.textContent = answers[i];
+    answerButtons.onclick = document //function to be made later it is going to check if somthing is correct if it is () => checkQuestion(answers[i])
+      .querySelector("#answerButtons")
+      .appendChild(answerButtons)
+      .classList.add("buttonClass");
+
     console.log("CONSOLELOGA4");
   }
 }
-var checkQuestion = function () {};
-function wrongAnswer() {}
+var correctAnswers;
+
+var addTimeIfCorrect = function () {
+  if (correctAnswers === true) {
+    timeLeftCountdown + 10000;
+  }
+};
+//?? answerButtons
+//??   .getElementsByClassName("buttonClass")
+//??   .addEventListener("click", function () {
+//??     console.log("answer clicked");
+//??   });ddd
+//TODO the div with the questions is loads on page loads and it is hidden until the user click the start button so i looks like the start button is starting the questions but it is not it is just unhiding the questions
 
 // REFERENCEvar askQuestionEl = document.createElement("li");
 //TODO inside of next quesion we want to give each answer a button and put it on the screen.
 
 //TODO each button should have  click listener that handles answering a question
 
-//TODO the click listener should handles scores and penalties then handle moving on to the next question
+//// the click listener should handles scores and penalties then handle moving on to the next question
 
 //TODO if there are nomore questions the listener should call a function that handles ending the game
 
